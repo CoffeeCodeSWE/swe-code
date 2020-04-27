4af469640971480820351b16785098720fd9fa61
Enrico Buratto <enrico.buratto.1@studenti.unipd.it>
2020-04-27 21:05:30 +0200

    feat: aggiungi calcolo regressione
    spaghetti code :)
    
    close #17

--- training-app/js/RL-calculate.js
+++ training-app/js/RL-calculate.js
@@ -1,3 +1,5 @@
+//versione molto primitiva, but it...just works
+
 const Regression = require('../vendor/regression');
 const fs = require('fs');
 const path = require('path');
@@ -5,13 +7,26 @@ const path = require('path');
 //qui cambiare la directory con quella dove arrivano i file in input
 let rawdata = fs.readFileSync(path.resolve(__dirname, "../js-examples/1-input-RL.json"));
 let data = JSON.parse(rawdata);
+//POST=(dentro a data c'è il file json, però in forma di oggetto)
 
-//per prendere i valori della prima
-console.log(data.variables.x);
+var numVar =Object.keys(data.variables).length;
+var numArr =(data.variables[Object.keys(data.variables)[0]]).length;
+//POST=(numVar contiene il numero di variabili)
+//POST=(numArr contiene il numero di parametri di ogni variabile (sempre uguale))
 
+var reg = new Regression({ numX: numVar+1, numY: 1 })
 
-//calcolo dei predittori
+//PRE=(niente in mano)
+for(let j=0; j<numArr; ++j) { 
+  let A = [];
+  A.push(1);
+  for(let i =0; i<numVar; ++i) {
+    A.push(data.variables[Object.keys(data.variables)[i]][j]);
+  }
+  let yNow =[];
+  yNow.push(data.target[Object.keys(data.target)][j]);
+  reg.push({x: A, y: yNow});
+}
+//POST=(ha calcolato tutto)
 
-//con eventuale caricamento del vecchio json e del nuovo csv
-
-//scrittura dei json di output
+console.log(reg.calculateCoefficients());
