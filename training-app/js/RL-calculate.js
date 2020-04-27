04972a24637e2b9754dc7d29e073e84127ac02c7
Enrico Buratto <enrico.buratto.1@studenti.unipd.it>
2020-04-27 15:47:04 +0200

    feat: aggiungi lettura file json

--- training-app/js/RL-calculate.js
+++ training-app/js/RL-calculate.js
@@ -1,1 +1,17 @@
 const Regression = require('../vendor/regression');
+const fs = require('fs');
+const path = require('path');
+
+//qui cambiare la directory con quella dove arrivano i file in input
+let rawdata = fs.readFileSync(path.resolve(__dirname, "../js-examples/1-input-RL.json"));
+let data = JSON.parse(rawdata);
+
+//per prendere i valori della prima
+console.log(data.variables.x);
+
+
+//calcolo dei predittori
+
+//con eventuale caricamento del vecchio json e del nuovo csv
+
+//scrittura dei json di output
