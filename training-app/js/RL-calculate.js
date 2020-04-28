d745a52027a5d199ca89c857c3120b831669988f
Enrico Buratto <enrico.buratto.1@studenti.unipd.it>
2020-04-28 19:14:10 +0200

    refactor: sistema codice a funzioni + aggiungi outuput

--- training-app/js/RL-calculate.js
+++ training-app/js/RL-calculate.js
@@ -1,32 +1,71 @@
-//versione molto primitiva, but it...just works
+'use strict';
 
 const Regression = require('../vendor/regression');
 const fs = require('fs');
 const path = require('path');
 
-//qui cambiare la directory con quella dove arrivano i file in input
-let rawdata = fs.readFileSync(path.resolve(__dirname, "../js-examples/1-input-RL.json"));
-let data = JSON.parse(rawdata);
-//POST=(dentro a data c'è il file json, però in forma di oggetto)
-
-var numVar =Object.keys(data.variables).length;
-var numArr =(data.variables[Object.keys(data.variables)[0]]).length;
-//POST=(numVar contiene il numero di variabili)
-//POST=(numArr contiene il numero di parametri di ogni variabile (sempre uguale))
-
-var reg = new Regression({ numX: numVar+1, numY: 1 })
-
-//PRE=(niente in mano)
-for(let j=0; j<numArr; ++j) { 
-  let A = [];
-  A.push(1);
-  for(let i =0; i<numVar; ++i) {
-    A.push(data.variables[Object.keys(data.variables)[i]][j]);
-  }
-  let yNow =[];
-  yNow.push(data.target[Object.keys(data.target)][j]);
-  reg.push({x: A, y: yNow});
+function readJson() {
+  let jsPath = path.resolve(__dirname, '../js-examples/1-input-RL.json');
+  let rawdata = fs.readFileSync(jsPath);
+  return JSON.parse(rawdata);
 }
-//POST=(ha calcolato tutto)
 
-console.log(reg.calculateCoefficients());
+function readOutJson() {
+  let jsPath = path.resolve(__dirname, '')
+}
+
+function calculateMatrixDimensions(data) {
+  let aux = data.variables;
+  let columns = Object.keys(aux).length;
+  let rows = (aux[Object.keys(aux)[0]]).length;
+  return {'columns' : columns+1, 'rows' : rows};
+}
+
+function libAdaptation(data, matrix, reg) {
+  for(let i=0; i<matrix.rows; ++i) { 
+    let A = [];
+    A.push(1);
+
+    for(let j = 0; j < matrix.columns-1; ++j) {
+      A.push(data.variables[Object.keys(data.variables)[j]][i]);
+    }
+
+    let yNow =[];
+    yNow.push(data.target[Object.keys(data.target)][i]);
+    reg.push({x: A, y: yNow});
+  }
+  return reg;
+}
+
+function rlFromScratch() {
+  let data = readJson();
+  let matrix = calculateMatrixDimensions(data);
+  var reg = new Regression({numX: matrix.columns , numY: 1});
+  return libAdaptation(data, matrix, reg);
+}
+
+function writeJson(reg) {
+  let tuple = (Object.keys(reg).length) - 1;
+  let intercept = reg[0];
+
+  let aux = readJson().variables;
+  let toN = Object.keys(aux).length;
+  let variablesNames = [];
+  for(let i=0; i < toN ; ++i) {
+    variablesNames.push((Object.keys(aux))[i]);
+  }
+
+  let coefficents = {};
+  for(let i=0; i<toN; ++i) {
+    coefficents[variablesNames[i]] = reg[i+1];
+  }
+
+  let obj = {'tuple' : tuple , 'coefficents' : coefficents, 'intercept' : intercept};
+  obj = JSON.stringify(obj);
+  fs.writeFileSync('file.json', obj);
+}
+
+
+
+var reg = rlFromScratch();
+writeJson(reg.calculateCoefficients());
